1d6700da6fcc707a72182fa7d084493a9c75167a
Jordan Tran <jordantran@ucsb.edu>
2022-01-10 00:05:38 -0800

    Initial Commit

---
+++ Emogen/EmogenApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmogenApp.swift
+//  Emogen
+//
+//  Created by Jordan on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct EmogenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
